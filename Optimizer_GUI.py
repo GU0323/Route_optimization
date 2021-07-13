00cb6a94e018c25fca6fee470058979845f01860
Kimjongkyu <Jong0206>
2021-07-14 02:46:46 +0900

    제약조건 및 오류 수정, 초기값 추가(노드:40 사용)

--- Optimizer_GUI.py
+++ Optimizer_GUI.py
@@ -2,159 +2,20 @@ from PyQt5.QtWebEngineWidgets import QWebEngineView
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
-from PyQt5.QtGui import *
-from PyQt5.QtCore import Qt
 from jmetal.algorithm.multiobjective.nsgaii import NSGAII
 from jmetal.operator import SBXCrossover, PolynomialMutation
 from jmetal.util.comparator import DominanceComparator
 from jmetal.util.solution import print_function_values_to_file, print_variables_to_file
 from jmetal.util.termination_criterion import StoppingByEvaluations
-import numpy as np
-import map_array
 import io
 import folium
+from math import cos, sin
+from defined_problem import Linear4
 
-from math import sqrt, cos, sin, atan, acos, pi
 
-from jmetal.core.problem import BinaryProblem, FloatProblem
-from jmetal.core.solution import BinarySolution, FloatSolution
 
 
 form_ui = uic.loadUiType("./GUI/optimizer_route.ui")[0]
-
-maze = map_array.map
-
-
-'''
-Con = np.where(maze > 0)
-
-
-INDEX = Con[0]
-COLUMN = Con[1]
-index_list = INDEX.tolist()
-column_list = COLUMN.tolist()
-index_list2 = []
-column_list2 = []
-
-
-
-
-for i in range(len(index_list)):
-    index_value = index_list[i] + e
-    index_list2.append(index_value)
-
-
-for i in range(len(column_list)):
-    column_value = column_list[i] + e
-    column_list2.append(column_value)
-'''
-
-class Linear4(FloatProblem):
-
-    def __init__(self, number_of_variables, x1, x2, y1, y2, lowerbound_v, lowerbound_t, upperbound_v, upperbound_t):
-        super(Linear4, self).__init__()
-        self.number_of_objectives = 1
-        self.number_of_variables = number_of_variables
-        self.number_of_constraints = int(number_of_variables / 2)
-        self.obj_directions = [self.MINIMIZE]
-        self.lower_bound = [lowerbound_v, lowerbound_t] * (int(number_of_variables / 2))
-        self.upper_bound = [upperbound_v, upperbound_t] * (int(number_of_variables / 2))
-        self.Px_departure = x1
-        self.Py_departure = y1
-        self.Px_arrival = x2
-        self.Py_arrival = y2
-
-
-
-    def evaluate(self, solution: FloatSolution) -> FloatSolution:
-
-        distance = 0
-        S = solution.variables
-        distance_list = []
-
-        del_t = 6
-        Px_arrival = 1799 + self.Px_arrival * 10
-        Py_arrival = 899 + self.Py_arrival * 10
-        Px_departure = 1799 + self.Px_departure * 10
-        Py_departure = 899 + self.Py_departure * 10
-
-        x = []
-        y = []
-        x.append(Px_departure)
-        y.append(Py_departure)
-        R_diviation = 0
-
-        for i in range(int(solution.number_of_variables / 2) - 1):
-            distance += S[2 * i] * del_t
-            distance_list.append(S[2 * i] * del_t)
-            A = x[i] + S[2 * i] * del_t * cos(S[2 * i + 1])
-            B = y[i] + S[2 * i] * del_t * sin(S[2 * i + 1])
-
-            x.append(A)
-            y.append(B)
-
-
-        R = sqrt((Px_arrival - x[-1]) ** 2 + (Py_arrival - y[-1]) ** 2)
-        distance_list.append(R)
-        x.append(Px_arrival)
-        y.append(Py_arrival)
-
-
-
-        for i in range(int(solution.number_of_variables / 2)):
-            R_mean = distance / int(solution.number_of_variables / 2)
-
-            R_diviation += sqrt((distance_list[i] - R_mean) ** 2)
-
-        distance = distance + R + R_diviation
-
-        solution.objectives[0] = distance
-        self.evaluate_constraints(solution)
-
-        return solution
-
-    def evaluate_constraints(self, solution: FloatSolution) -> None:
-        constraints = [0.0 for _ in range(self.number_of_constraints)]
-        S = solution.variables
-
-        del_t = 6
-        x = []
-        y = []
-        Px_arrival = 1799 + self.Px_arrival * 10
-        Py_arrival = 899 + self.Py_arrival * 10
-        Px_departure = 1799 + self.Px_departure * 10
-        Py_departure = 899 + self.Py_departure * 10
-        x.append(Px_departure)
-        y.append(Py_departure)
-
-        for i in range(int(solution.number_of_variables / 2) - 1):
-            A = x[i] + S[2 * i] * del_t * cos(S[2 * i + 1])
-            B = y[i] + S[2 * i] * del_t * sin(S[2 * i + 1])
-
-            x.append(A)
-            y.append(B)
-
-        x.append(Px_arrival)
-        y.append(Py_arrival)
-
-        for i in range(int(solution.number_of_variables / 2)):
-
-            # if maze[int(1799-y[i+1]-1)][int(x[i+1]-1)] == 1:
-
-            # if maze[int(1799-y[i+1]-1)][int(x[i+1]-1)] == 1:
-
-
-            if maze[int(1799 - y[i + 1] + 1)][int(x[i + 1] + 1)] == 1 or maze[int(1799 - y[i + 1] - 1)][int(x[i + 1] - 1)] == 1 or maze[int(1799 - y[i + 1])][int(x[i + 1])] == 1 or \
-                    maze[int(1799 - y[i + 1] + 2)][int(x[i + 1] + 2)] == 1 or maze[int(1799 - y[i + 1] - 2)][int(x[i + 1] - 2)]:
-                constraints[i] = -100
-
-            # if round(x[i + 1]) in self.column_list and round(1799 - y[i + 1]) in self.index_list or (1799 - y[i + 1]) >= 1800 or (1799 - y[i + 1]) < 0 or (1799 < x[i + 1]):
-
-        solution.constraints = constraints
-
-    def get_name(self) -> str:
-        return 'Linear4'
-
 
 class MyWindow(QMainWindow, form_ui):
 
@@ -175,6 +36,15 @@ class MyWindow(QMainWindow, form_ui):
         self.x2 = []
         self.y2 = []
         self.webView = QWebEngineView()
+        self.m = folium.Map(
+            location=[0, 0],
+            tiles='Stamen Terrain',
+            zoom_start=2
+        )
+
+        data = io.BytesIO()
+        self.m.save(data, close_file=False)
+        self.webView.setHtml(data.getvalue().decode())
         self.verticalLayout.addWidget(self.webView)
 
         self.departure.activated[str].connect(self.onActedate_departure)
@@ -251,13 +121,13 @@ class MyWindow(QMainWindow, form_ui):
         front = algorithm.get_result()
 
         # Save results to file
-        print_function_values_to_file(front, 'FUN3_부산_호치민(40)_map1.' + algorithm.get_name() + "-" + problem.get_name())
-        print_variables_to_file(front, 'VAR3_linear_부산_호치민(40)_map1.' + algorithm.get_name() + "-" + problem.get_name())
+        print_function_values_to_file(front, 'FUN3_부산_호치민(node=40).' + algorithm.get_name() + "-" + problem.get_name())
+        print_variables_to_file(front, 'VAR3_linear_부산_호치민(node=40).' + algorithm.get_name() + "-" + problem.get_name())
 
-        file_open = open('VAR3_linear_부산_호치민(40)_map1.' + algorithm.get_name() + "-" + problem.get_name(), 'r',
+        file_open = open('VAR3_linear_부산_호치민(node=40).' + algorithm.get_name() + "-" + problem.get_name(), 'r',
                          encoding='utf-8')
 
-        file_open2 = open('FUN3_부산_호치민(40)_map1.' + algorithm.get_name() + "-" + problem.get_name(), 'r',
+        file_open2 = open('FUN3_부산_호치민(node=40).' + algorithm.get_name() + "-" + problem.get_name(), 'r',
                           encoding='utf-8')
         obj_line = file_open2.readlines()
         obj = obj_line[0]
@@ -265,18 +135,19 @@ class MyWindow(QMainWindow, form_ui):
         file_open2.close()
 
         line = file_open.readlines()
-        word = line[-1].split(" ")
+        word = line[0].split(" ")
         x = []
         y = []
         Px_arrival = 1799 + self.arrival_lon * 10
         Py_arrival = 899 + self.arrival_lat * 10
         Px_departure = 1799 + self.departure_lon * 10
         Py_departure = 899 + self.departure_lat * 10
+
         x.append(Px_departure)
         y.append(Py_departure)
         del_t = 6
 
-        for i in range(int(self.Node / 2 - 1)):
+        for i in range(int(self.Node / 2)-1):
             xi = x[i] + float(word[2 * i]) * del_t * cos(float(word[2 * i + 1]))
             yi = y[i] + float(word[2 * i]) * del_t * sin(float(word[2 * i + 1]))
 
@@ -290,8 +161,9 @@ class MyWindow(QMainWindow, form_ui):
             print(x[i], y[i])
 
         for i in range(len(x)):
-            py = (y[i] - 899) / 10
-            px = (x[i] - 1799) / 10
+            py = (y[i]-899) / 10
+            px = (x[i]-1799) / 10
+
             self.x2.append(px)
             self.y2.append(py)
 
@@ -351,8 +223,6 @@ class MyWindow(QMainWindow, form_ui):
         data = io.BytesIO()
         self.m.save(data, close_file=False)
         self.webView.setHtml(data.getvalue().decode())
-
-
 
 
 
--- map_array.py
+++ map_array.py
@@ -1,21 +1,21 @@
 import cv2
 import numpy as np
 
-image = cv2.imread('map_F.png', cv2.IMREAD_COLOR)
+image = cv2.imread('./resources/mapImage/worldmap.png', cv2.IMREAD_COLOR)
 
-map = np.zeros((1800, 5400), dtype = image.dtype)
+map = np.zeros((1800, 3600), dtype = image.dtype)
 
 map_height, map_width = 2, 2
 
 for j in range(1800):
-    for i in range(5400):
+    for i in range(3600):
         x = i*map_width
         y = j*map_height
 
         pixel = image[y:y+map_height, x:x+map_width]
         map[j,i] = cv2.mean(pixel)[0]
 
-map = np.where(map<10,int(1),map)
-map = np.where(map>=10,int(0),map)
+map = np.where(map<255,int(1),map)
+map = np.where(map>=255,int(0),map)
 
-np.savetxt('maparray.txt', map, fmt='%1.0f')
+np.save('maparray', map)
